d07f9897216953bdd1609937bf2b109608e45522
Yovie Alfa Guistuta <yoviealfaguistuta@gmail.com>
2022-08-29 10:58:08 +0700

    Fixing ukm migration

--- database/migrations/2022_08_01_104808_create_ukm_table.php
+++ database/migrations/2022_08_01_104808_create_ukm_table.php
@@ -15,13 +15,17 @@ class CreateUkmTable extends Migration
     {
         Schema::create('ukm', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_ukm', 100);
+            $table->string('nama_ukm');
             $table->enum('jenis', ['ormawa', 'ukm']);
             $table->string('singkatan_ukm', 100);
-            $table->string('nama_ketua', 100);
-            $table->string('nama_wakil_ketua', 100);
-            $table->string('nama_sekertaris', 100);
-            $table->text('keterangan');
+            $table->string('nama_ketua');
+            $table->string('nama_wakil_ketua')->nullable();
+            $table->string('nama_sekertaris')->nullable();
+            $table->text('keterangan')->nullable();
+            $table->text('foto_ukm');
+            $table->text('foto_ketua');
+            $table->text('foto_wakil_ketua')->nullable();
+            $table->text('foto_sekertaris')->nullable();
             
             $table->timestamps();
         });
